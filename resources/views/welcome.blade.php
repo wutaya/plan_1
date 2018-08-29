5c0466cf20e0c8dde8f7b7254aec6a290b196f66
wutaya <wu1940@foxmail.com>
2018-08-29 11:28:01 +0800

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    WUQH
+                    HAHA
                 </div>
 
                 <div class="links">
