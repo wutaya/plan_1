b6e74562f1701603a835ae3616809c3351191981
wutaya <wu1940@foxmail.com>
2018-08-29 11:19:37 +0800

    webhook

---
+++ app/Http/Controllers/DeploymentController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DeploymentController extends Controller
+{
+    public function deploy(Request $request)
+    {
+        $commands = ['cd /var/www/html/plan_1', 'git pull'];
+        $signature = $request->header('X-Hub_signature');
+        $payload = file_get_contents('php://input');
+        if ($this->isFromGithub($payload, $signature)) {
+            foreach ($commands as $command) {
+                shell_exec($command);
+            }
+            http_response_code(200);
+        } else {
+            abort(403);
+        }
+    }
+
+    private function isFromGithub($payload, $signature)
+    {
+        return 'sha1=' . hash_hmac('sha1', $payload, env('GITHUB_DEPLOY_TOKEN', false)) === $signature;
+    }
+}
