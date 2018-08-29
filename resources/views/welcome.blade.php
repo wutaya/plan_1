4f537b6c08c2d61b2cca4b529f43a6feeda92ced
wutaya <wu1940@foxmail.com>
2018-08-29 11:45:55 +0800

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    HAHA
+                    dddddd
                 </div>
 
                 <div class="links">
