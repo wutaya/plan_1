5273549db1e6f7144675a80c6b42ec5c2b77f3cd
wutaya <wu1940@foxmail.com>
2018-09-07 09:56:53 +0800

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>个人开发测试网站</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
@@ -69,26 +69,26 @@
     @if (Route::has('login'))
         <div class="top-right links">
             @auth
-                <a href="{{ url('/home') }}">Home</a>
+                <a href="{{ url('/home') }}">主页</a>
             @else
-                <a href="{{ route('login') }}">Login</a>
-                <a href="{{ route('register') }}">Register</a>
+                <a href="{{ route('login') }}">登录</a>
+                <a href="{{ route('register') }}">注册</a>
             @endauth
         </div>
     @endif
 
     <div class="content">
         <div class="title m-b-md">
-            我的个人博客
+            个人开发测试网站
         </div>
 
         <div class="links">
-            <a href="https://laravel.com/docs">Documentation</a>
+            {{--<a href="https://laravel.com/docs">Documentation</a>
             <a href="https://laracasts.com">Laracasts</a>
             <a href="https://laravel-news.com">News</a>
             <a href="https://nova.laravel.com">Nova</a>
             <a href="https://forge.laravel.com">Forge</a>
-            <a href="https://github.com/laravel/laravel">GitHub</a>
+            <a href="https://github.com/laravel/laravel">GitHub</a>--}}
         </div>
     </div>
 </div>
